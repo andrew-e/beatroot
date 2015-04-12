5abf6bc5daf66cc096e38aea0d3db2c6b24c785c
Pote <willpote@gmail.com>
2015-04-12 14:15:47 +0100

    Added basic envelope

--- src/js/audio.js
+++ src/js/audio.js
@@ -5,6 +5,7 @@ var startTime = context.currentTime + 0.005;
 var noteTime = 0;
 var tempo = 60;
 var rhythmIndex = 0;
+var attackTime = 0.1;
 
 function schedule() {
   var time = context.currentTime - startTime;
@@ -13,8 +14,9 @@ function schedule() {
   // 0.2 acts as a schedule ahead time
   while (noteTime < time + 0.2) {
     var contextPlayTime = noteTime + startTime;
-    // Examine dom for active pads at the currently
-    // considered rhythmIndex and schedule 
+    // Examine DOM for active pads at current
+    // rhythmIndex and schedule notes 
+    // TODO: cache these jquery refs?
     $('.drum-machine tbody').children().each(function(i) {
       if ($(this).children().eq(rhythmIndex).hasClass('active')) {
         scheduleNote(i, contextPlayTime);
@@ -27,12 +29,22 @@ function schedule() {
 }
 
 function scheduleNote(row, time) {
-  // TODO: generate different frequency based on row
+  // TODO: Generic note generation
+  var BbMinor = [233.08, 261, 63, 277.18, 311.13, 
+      349.23, 369.99, 415.30, 466.16, 523.25, 
+      554.37, 622.25, 698.46, 739.99, 830.61, 
+      932.33, 1046.5];
   var oscillator = context.createOscillator();
-  oscillator.frequency.value = 200;
-  oscillator.connect(context.destination);
+  var gain = context.createGain();
+  gain.gain.setValueAtTime(0, context.currentTime);
+  gain.gain.linearRampToValueAtTime(1, time + attackTime);
+  gain.gain.linearRampToValueAtTime(0, time + (60 / tempo) * 0.25);
+
+  oscillator.frequency.value = BbMinor[row];
+  oscillator.connect(gain);
+  gain.connect(context.destination);
   oscillator.start(time);
-  oscillator.stop(time + (60 / tempo) * 0.25);
+  //oscillator.stop(time + (60 / tempo) * 0.25);
 }
 
 function advanceNote() {
